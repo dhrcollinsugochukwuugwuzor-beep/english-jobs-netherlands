347feddec88e8c0633640e7ffc6de644f074a914
dhrcollinsugochukwuugwuzor-beep <dhrcollinsugochukwuugwuzor@gmail.com>
2026-03-02 11:03:56 +0100

    Create App.js file for the main application

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import JobListings from './pages/JobListings';
+import JobDetail from './pages/JobDetail';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<JobListings />} />
+        <Route path="/jobs/:id" element={<JobDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
